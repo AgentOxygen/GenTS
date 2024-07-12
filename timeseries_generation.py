9a0f9b016a759449074e7a71cc9bf88be6830425
AgentOxygen <>
2024-07-12 12:45:47 -0500

    Merged components of new core function with faster old function
    The new "hybrid" is more efficient and (hopefully) stable

--- timeseries_generation.py
+++ timeseries_generation.py
@@ -96,87 +96,98 @@ class TSGenerationConfig:
         return batches
 
 
-def generate_timeseries(client, history_paths, output_template, overwrite=False, workers=[]):
+def generate_timeseries(client, output_template, batch_paths, overwrite=False):
+    logs = []
     Path(output_template).parent.mkdir(parents=True, exist_ok=True)
-    init_ds = client.submit(xarray.open_dataset, history_paths[0], chunks={}, decode_cf=False, workers=workers).result()
-    ds_variables = list(init_ds.variables)
 
+    with warnings.catch_warnings(action="ignore"):
+        history_concat = xarray.open_mfdataset(batch_paths,
+                                               parallel=True,
+                                               decode_cf=True,
+                                               data_vars="minimal",
+                                               combine='nested',
+                                               concat_dim="time",
+                                               compat="override",
+                                               coords="minimal")
+
+    dt = history_concat.time.values[1] - history_concat.time.values[0]
+    if dt.days == 0:
+        time_str_format = "%Y-%m-%d-%H"
+    elif 30 > dt.days > 0:
+        time_str_format = "%Y-%m-%d"
+    else:
+        time_str_format = "%Y-%m"
+
+    time_start = history_concat.time.values[0].strftime(time_str_format)
+    time_end = history_concat.time.values[-1].strftime(time_str_format)
+
+    ds_variables = list(history_concat.variables)
     output_variables = []
-    output_variables_drops = []
     auxillary_variables = []
     for target_variable in ds_variables:
-        dim_coords = np.unique(list(init_ds[target_variable].coords) + list(init_ds[target_variable].dims))
+        dim_coords = np.unique(list(history_concat[target_variable].coords) + list(history_concat[target_variable].dims))
         if len(dim_coords) > 1 and "time" in dim_coords and "nbnd" not in dim_coords and "chars" not in dim_coords:
             output_variables.append(target_variable)
-            output_variables_drops.append([variable for variable in ds_variables if variable not in dim_coords and variable != target_variable])
         else:
             auxillary_variables.append(target_variable)
 
-    def export_dataset(variable, variables_to_drop, paths, output_template, overwrite=False):
-        ds = xarray.open_mfdataset(paths,
-                                   decode_cf=True,
-                                   parallel=True,
-                                   data_vars="minimal",
-                                   concat_dim="time",
-                                   combine="nested",
-                                   drop_variables=variables_to_drop)
-        dt = ds.time.values[1] - ds.time.values[0]
-        if dt.days == 0:
-            time_str_format = "%Y-%m-%d-%H"
-        elif 30 > dt.days > 0:
-            time_str_format = "%Y-%m-%d"
-        else:
-            time_str_format = "%Y-%m"
-
-        time_start = ds.time.values[0].strftime(time_str_format)
-        time_end = ds.time.values[-1].strftime(time_str_format)
+    config_tuples = []
+    for variable in output_variables:
         output_path = f"{output_template}{variable}.{time_start}.{time_end}.nc"
-        if not isfile(output_path):
-            ds.to_netcdf(output_path, mode="w")
-        elif overwrite:
-            remove(output_path)
-            ds.to_netcdf(output_path, mode="w")
+        if not isfile(output_path) or overwrite:
+            config_tuples.append((
+                history_concat[[variable]],
+                output_path
+            ))
+        else:
+            logs.append("Skipping file because it already exists (assuming integrity checks were done already): ")
+            logs.append(f"\t '{output_path}'")
 
+    if len(config_tuples) == 0:
+        logs.append("Skipping group because all timeseries files already exists (assuming integrity checks were done already): ")
+        logs.append(f"\t '{output_template}'")
+        return logs
+
+    def write_base_timeseries(config_tuple):
+        ds, output_path = config_tuple
+        ds.to_netcdf(output_path, mode="w")
         return output_path
 
-    export_futures = client.map(export_dataset,
-                                output_variables,
-                                output_variables_drops,
-                                paths=history_paths,
-                                output_template=output_template,
-                                overwrite=overwrite)
-    export_paths = client.gather(export_futures)
-    del export_futures
+    base_ts_futures = client.map(write_base_timeseries, config_tuples)
 
-    attrs_ds = xarray.open_mfdataset(history_paths,
-                                     decode_cf=False,
-                                     parallel=True,
-                                     data_vars="minimal",
-                                     concat_dim="time",
-                                     combine="nested",
-                                     drop_variables=[v for v in ds_variables if v not in auxillary_variables]).compute()
+    aux_ds = history_concat[auxillary_variables].compute()
+
+    base_ts_paths = []
+    for future in base_ts_futures:
+        base_ts_paths.append(future.result())
+        future.release()
 
     if client.amm.running():
         client.amm.stop()
-    scatted_attrs = client.scatter(attrs_ds, broadcast=True)
+    scattered_aux = client.scatter(aux_ds, broadcast=True)
 
-    def add_aux_variables(path, ds):
+    def add_auxillary_variables(path, ds):
         ds.to_netcdf(path, mode="a")
+        return path
 
-    aux_futures = client.map(add_aux_variables, export_paths, [scatted_attrs]*len(export_paths))
-    client.gather(aux_futures)
+    add_aux_futures = client.map(add_auxillary_variables, base_ts_paths, ds=scattered_aux)
 
-    scatted_attrs.release()
+    add_aux_paths = []
+    for future in add_aux_futures:
+        add_aux_paths.append(future.result())
+        future.release()
+
+    scattered_aux.release()
     client.amm.start()
 
-    return True
+    return logs
 
 
 def generate_timeseries_batches(client, batches, verbose=False, overwrite=False):
     for index, (output_template, paths) in enumerate(batches):
         print(f"\nGenerating timeseries datasets for '{output_template}'", end="")
         start = time()
-        logs = generate_timeseries(client, paths, output_template, overwrite=overwrite)
+        logs = generate_timeseries(client, output_template, paths, overwrite=overwrite)
         print(f" ... done! {round(time() - start, 2)}s ({index+1}/{len(batches)})")
         if verbose:
             print(f"\t[Verbose=True, {len(logs)} log messages]")
