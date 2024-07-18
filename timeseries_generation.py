21e5100384137dfef3c857f3d8fa71c791804223
AgentOxygen <cameronscummins@gmail.com>
2024-07-17 23:43:56 -0500

    Fixed bug with too many open file handles

--- timeseries_generation.py
+++ timeseries_generation.py
@@ -12,6 +12,122 @@ import netCDF4
 import cftime
 
 
+class TimeSeriesOrder:
+    def __init__(self, output_path_template, history_file_paths):
+        self.__output_path_template = output_path_template
+        self.__history_files_paths = history_file_paths
+
+        full_ds = netCDF4.MFDataset(self.__history_files_paths, check=False, aggdim="time")
+        self.__variables = list(full_ds.variables)
+        self.__primary_variables = []
+        self.__auxillary_variables = []
+        for var_index, target_variable in enumerate(self.__variables):
+            dim_coords = np.unique(list(full_ds[target_variable].dimensions))
+            if len(dim_coords) > 1 and "time" in dim_coords and "nbnd" not in dim_coords and "chars" not in dim_coords:
+                self.__primary_variables.append(target_variable)
+            else:
+                self.__auxillary_variables.append(target_variable)
+
+        self.__time_raw = full_ds["time"][:]
+        self.__time_units = full_ds["time"].units
+        self.__time = cftime.num2date(self.__time_raw, self.__time_units)
+        full_ds.close()
+
+        self.__history_file_times = []
+        for path in history_file_paths:
+            ds = netCDF4.Dataset(path)
+            self.__history_file_times.append(ds["time"][:])
+            ds.close()
+
+        self.__time_path_indices = self.indexTimestampsToPaths(self.__history_files_paths, self.__time_raw)
+        self.__time_step = self.getTimeStep()
+        self.__time_str_format = self.getTimeStrFormat(self.__time_step)
+        self.__history_file_groupings = []
+        self.__ts_output_tuples = []
+        self.__chunk_year_length = None
+        self.generateOutputs()
+
+    def getIndices(self):
+        return self.__time_path_indices
+
+    def indexTimestampsToPaths(self, paths, concat_times):
+        index_to_path = []
+        for index in range(len(concat_times)):
+            path_index = None
+            for times in self.__history_file_times:
+                if concat_times[index] in times:
+                    path_index = index
+                    break
+            index_to_path.append(path_index)
+        return index_to_path
+
+    def generateOutputs(self):
+        ts_slices = []
+        yr_start = 0
+        if self.__chunk_year_length is not None:
+            for index in range(len(self.__time)):
+                if self.__time[index].year >= self.__time[yr_start].year + self.__chunk_year_length:
+                    ts_slices.append((yr_start, index))
+                    yr_start = index
+        else:
+            ts_slices.append((0, len(self.__time)))
+
+        input_hist_file_tuples = []
+        for start_index, end_index in ts_slices:
+            path_start_index = self.__time_path_indices[start_index]
+            path_end_index = self.__time_path_indices[end_index-1]
+            input_hist_file_tuples.append((self.__time[start_index], self.__time[end_index-1], self.__history_files_paths[path_start_index:path_end_index]))
+
+        self.__history_file_groupings = input_hist_file_tuples
+
+        for start_time, end_time, paths in input_hist_file_tuples:
+            for variable in self.__primary_variables:
+                self.__ts_output_tuples.append((self.__auxillary_variables + [variable], self.__output_path_template, start_time, end_time, paths))
+
+    def getCommandStrings(self):
+        cmds = []
+        for variables, template, start_time, end_time, paths in self.__ts_output_tuples:
+            path_str = ""
+            for path in paths:
+                path_str += f"{str(path)} "
+
+            var_str = ""
+            for var_i in variables:
+                var_str += f"{var_i},"
+
+            start_time_str = start_time.strftime(self.__time_str_format)
+            end_time_str = end_time.strftime(self.__time_str_format)
+
+            cmd = "ncrcat -v " + var_str[:-1] + f" {path_str[:-1]}" + f" -O {template}.{variables[-1]}.{start_time_str}.{end_time_str}.nc"
+            cmds.append(cmd)
+        return cmds
+
+    def getAllHistoryFilePaths(self):
+        return self.__history_files_paths
+
+    def getTimeStrFormat(self, time_step_label):
+        if "hour" in time_step_label:
+            time_str_format = "%Y-%m-%d-%H"
+        elif "day" in time_step_label:
+            time_str_format = "%Y-%m-%d"
+        elif "month" in time_step_label:
+            time_str_format = "%Y-%m"
+        else:
+            time_str_format = "%Y"
+        return time_str_format
+
+    def getTimeStep(self):
+        dt_hrs = (self.__time[1] - self.__time[0]).total_seconds() / 60 / 60
+        if dt_hrs >= 24*365:
+            return f"year_{int(dt_hrs / (24*365))}"
+        elif 24*31 >= dt_hrs >= 24*30:
+            return f"month_{int(dt_hrs / (24*30))}"
+        elif dt_hrs >= 24:
+            return f"day_{int(dt_hrs / (24))}"
+        else:
+            return f"hour_{int(dt_hrs)}"
+
+
 class TimeSeriesConfig:
     def __init__(self, input_head_dir, output_head_dir, directory_name_swaps={}, timestep_directory_names={}, file_name_exclusions=[], directory_name_exclusions=[]):
         input_head_dir = Path(input_head_dir)
@@ -64,118 +180,13 @@ class TimeSeriesConfig:
                 parent_output = parent_output.replace(f"/{keyword}", f"/{directory_name_swaps[keyword]}")
 
             self.ts_output_groups[parent_output] = prefix_groups
-            self.output_orders = self.get_output_orders()
 
         self.ts_orders = []
         for parent_directory in self.ts_output_groups:
             for prefix in self.ts_output_groups[parent_directory]:
+                print(f"{parent_directory}/{prefix}")
                 self.ts_orders.append(TimeSeriesOrder(f"{parent_directory}/{prefix}", self.ts_output_groups[parent_directory][prefix]))
 
-
-class TimeSeriesOrder:
-    def __init__(self, output_path_template, history_file_paths):
-        self.__output_path_template = output_path_template
-        self.__history_files_paths = history_file_paths
-        self.__mfdataset = netCDF4.MFDataset(history_file_paths, check=False, aggdim="time")
-        self.__time = cftime.num2date(self.__mfdataset["time"][:], self.__mfdataset["time"].units)
-        self.__time_path_indices = self.indexTimestampsToPaths(self.__history_files_paths, self.__mfdataset["time"][:])
-        self.__time_step = self.getTimeStep()
-        self.__time_str_format = self.getTimeStrFormat(self.__time_step)
-        self.__history_file_groupings = []
-        self.__ts_output_tuples = []
-        self.__chunk_year_length = None
-        self.generateOutputs()
-
-    def getIndices(self):
-        return self.__time_path_indices
-
-    def indexTimestampsToPaths(self, paths, concat_times):
-        dataset_times = [netCDF4.Dataset(path)["time"][:] for path in paths]
-        index_to_path = []
-        for index in range(len(concat_times)):
-            path_index = None
-            for times in dataset_times:
-                if concat_times[index] in times:
-                    path_index = index
-                    break
-            index_to_path.append(path_index)
-        return index_to_path
-
-    def generateOutputs(self):
-        ts_slices = []
-        yr_start = 0
-        if self.__chunk_year_length is not None:
-            for index in range(len(self.__time)):
-                if self.__time[index].year >= self.__time[yr_start].year + self.__chunk_year_length:
-                    ts_slices.append((yr_start, index))
-                    yr_start = index
-        else:
-            ts_slices.append((0, len(self.__time)))
-
-        input_hist_file_tuples = []
-        for start_index, end_index in ts_slices:
-            path_start_index = self.__time_path_indices[start_index]
-            path_end_index = self.__time_path_indices[end_index-1]
-            input_hist_file_tuples.append((self.__time[start_index], self.__time[end_index-1], self.__history_files_paths[path_start_index:path_end_index]))
-
-        self.__history_file_groupings = input_hist_file_tuples
-
-        variables = list(self.__mfdataset.variables)
-        primary_variables = []
-        auxillary_variables = []
-        for var_index, target_variable in enumerate(variables):
-            dim_coords = np.unique(list(self.__mfdataset[target_variable].dimensions))
-            if len(dim_coords) > 1 and "time" in dim_coords and "nbnd" not in dim_coords and "chars" not in dim_coords:
-                primary_variables.append(target_variable)
-            else:
-                auxillary_variables.append(target_variable)
-
-        for start_time, end_time, paths in input_hist_file_tuples:
-            for variable in primary_variables:
-                self.__ts_output_tuples.append((auxillary_variables + [variable], self.__output_path_template, start_time, end_time, paths))
-
-    def getCommandStrings(self):
-        cmds = []
-        for variables, template, start_time, end_time, paths in self.__ts_output_tuples:
-            path_str = ""
-            for path in paths:
-                path_str += f"{str(path)} "
-
-            var_str = ""
-            for var_i in variables:
-                var_str += f"{var_i},"
-
-            start_time_str = start_time.strftime(self.__time_str_format)
-            end_time_str = end_time.strftime(self.__time_str_format)
-
-            cmd = "ncrcat -v " + var_str[:-1] + f" {path_str[:-1]}" + f" -O {template}.{variables[-1]}.{start_time_str}.{end_time_str}.nc"
-            cmds.append(cmd)
-        return cmds
-
-    def getAllHistoryFilePaths(self):
-        return self.__history_files_paths
-
-    def getTimeStrFormat(self, time_step_label):
-        if "hour" in time_step_label:
-            time_str_format = "%Y-%m-%d-%H"
-        elif "day" in time_step_label:
-            time_str_format = "%Y-%m-%d"
-        elif "month" in time_step_label:
-            time_str_format = "%Y-%m"
-        else:
-            time_str_format = "%Y"
-        return time_str_format
-
-    def getTimeStep(self):
-        dt_hrs = (self.__time[1] - self.__time[0]).total_seconds() / 60 / 60
-        if dt_hrs >= 24*365:
-            return f"year_{int(dt_hrs / (24*365))}"
-        elif 24*31 >= dt_hrs >= 24*30:
-            return f"month_{int(dt_hrs / (24*30))}"
-        elif dt_hrs >= 24:
-            return f"day_{int(dt_hrs / (24))}"
-        else:
-            return f"hour_{int(dt_hrs)}"
 
 
 def generate_timeseries(client, output_template, hist_paths, overwrite=False):
