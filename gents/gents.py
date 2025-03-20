364ec716b476917993d0f2e135e706e1c6bb22c9
AgentOxygen <cameron.cummins@utexas.edu>
2025-03-20 10:45:35 -0500

    Separated out main gents function

--- gents/gents.py
+++ gents/gents.py
@@ -10,16 +10,14 @@ Contact: cameron.cummins@utexas.edu
 Last Header Update: 3/5/24
 """
 import dask
-from gents.read import get_groups_from_path, generate_output_template
+from gents.read import *
 from gents.timeseries import generate_time_series
 
 
-def generate_time_series_from_directory(input_head_dir, output_head_dir, complevel=0, compression=None, overwrite=True, dask_client=None):
+def generate_time_series_from_meta_groups(group_metas, input_head_dir, output_head_dir, complevel=0, compression=None, overwrite=True, dask_client=None):
     if dask_client is None:
         dask_client = dask.distributed.client._get_global_client()
 
-    group_metas = get_groups_from_path(input_head_dir, dask_client=dask_client)
-    
     dask_gents = []
     for group_id in list(group_metas.keys()):
         template_path = generate_output_template(input_head_dir, group_id, output_head_dir=output_head_dir)
@@ -35,3 +33,27 @@ def generate_time_series_from_directory(input_head_dir, output_head_dir, complev
         dask_gents.append(delayed_func)
     
     ts_paths = dask.compute(*dask_gents)
+
+
+def generate_time_series_from_directory(input_head_dir, output_head_dir, gents_config=None, complevel=0, compression=None, overwrite=True, dask_client=None, slice_size_years=10):
+    if dask_client is None:
+        dask_client = dask.distributed.client._get_global_client()
+
+    if gents_config is None:
+        gents_config = get_default_config()
+    
+    paths = find_files(input_head_dir, "*.nc")
+    paths = [path for path in paths if "/rest/" not in str(path.parent)]
+
+    check_config(gents_config)
+    path_to_meta_map = get_metas_from_paths(paths, dask_client=dask_client)
+    if gents_config["include"] is not None:
+        path_to_meta_map = apply_inclusive_filters(path_to_meta_map, custom_filter["include"])
+    if gents_config["exclude"] is not None:
+        path_to_meta_map = apply_exclusive_filters(path_to_meta_map, custom_filter["exclude"])
+
+    groups = sort_hf_groups(list(path_to_meta_map.keys()))
+    sliced_groups = slice_hf_groups(groups, path_to_meta_map, slice_size_years)
+    group_metas = check_groups_by_variables(sliced_groups)
+    
+    generate_time_series_from_meta_groups(group_metas, input_head_dir, output_head_dir, complevel, compression, overwrite, dask_client)
