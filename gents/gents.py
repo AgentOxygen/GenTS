f1b41620d118032957f62df0eb0ed86914ee4fb3
AgentOxygen <cameron.cummins@utexas.edu>
2025-04-30 22:16:36 -0500

    Created entry point function using HFCollection

--- gents/gents.py
+++ gents/gents.py
@@ -10,112 +10,47 @@ Contact: cameron.cummins@utexas.edu
 Last Header Update: 3/5/24
 """
 import dask
-from gents.read import *
-from gents.timeseries import generate_time_series, is_var_secondary
+from gents.timeseries import generate_time_series
+from gents.hfcollection import HFCollection
+from os import makedirs
+from pathlib import Path
 
 
-def generate_time_series_from_meta_groups(group_metas, input_head_dir, output_head_dir, parallel_by_vars=False, complevel=0, compression=None, overwrite=True, dask_client=None):
-    """
-    Generates time series files from history file groups defined by metadata class objects.
-
-    :param group_metas: Dictionary mapping group IDs (key) to history file metadata objects (value).
-    :param input_head_dir: Head directory to history file repository.
-    :param output_head_dir: Head directory to write time series files and subdirectory structure to.
-    :param parallel_by_vars: Parallelize by primary variables. Can have substantial performance uplift, but not well tested for netCDF3.
-    :param complevel: Compression level to apply
-    :param compression: Compression algorithm to use.
-    :param overwrite: Whether or not to overwrite existing time series files.
-    :param dask_client: Dask client to use, defaults to global client if unspecified.
-    :return: Paths to time series files generated.
-    """
+def generate_ts_from_hfcollection(hf_collection: HFCollection,
+                                  output_dir: str,
+                                  complevel=0,
+                                  compression=None,
+                                  overwrite=False,
+                                  dir_swaps={"/hist/":"/t_series/"},
+                                  dask_client=None
+                                 ) -> list:
     if dask_client is None:
         dask_client = dask.distributed.client._get_global_client()
 
     dask_gents = []
-    for group_id in list(group_metas.keys()):
-        template_path = generate_output_template(input_head_dir, group_id, output_head_dir=output_head_dir)
-        group_hf_paths = [meta_ds.get_path() for meta_ds in group_metas[group_id]]
-        if parallel_by_vars:
-            for variable in group_metas[group_id][0].get_variables():
-                delayed_func = dask.delayed(generate_time_series)(
-                    group_hf_paths,
-                    template_path.parent,
-                    template_path.name,
-                    complevel,
-                    compression,
-                    overwrite,
-                    variable
-                )
-                dask_gents.append(delayed_func)
-        else:
+    hf_groups = hf_collection.slice_groups()
+    for glob_template in hf_groups:
+        hf_paths = hf_groups[glob_template]
+        
+        output_template = glob_template.split(hf_collection.get_input_dir())[1]
+        ts_path_template = f"{output_dir}{output_template}"
+        for key in dir_swaps:
+            ts_path_template = ts_path_template.replace(key, dir_swaps[key])
+        ts_path_template = ts_path_template.split("*")[0]
+
+        makedirs(Path(ts_path_template).parent, exist_ok=True)
+        
+        primary_vars = hf_collection[hf_paths[0]].get_primary_variables()
+        secondary_vars = hf_collection[hf_paths[0]].get_secondary_variables()
+        for variable in primary_vars:
             delayed_func = dask.delayed(generate_time_series)(
-                group_hf_paths,
-                template_path.parent,
-                template_path.name,
+                hf_paths,
+                ts_path_template,
+                [variable],
+                secondary_vars,
                 complevel,
                 compression,
-                overwrite,
-                None
+                overwrite
             )
             dask_gents.append(delayed_func)
-    ts_paths = dask.compute(*dask_gents)
-    return ts_paths
-
-
-def generate_time_series_from_directory(input_head_dir, output_head_dir, gents_config=None, parallel_by_vars=False, complevel=0, compression=None, overwrite=True, dask_client=None, slice_size_years=10):
-    if dask_client is None:
-        dask_client = dask.distributed.client._get_global_client()
-
-    if gents_config is None:
-        gents_config = get_default_config()
-    
-    paths = find_files(input_head_dir, "*.nc")
-
-    if gents_config["exclude"] is not None:
-        filtered_paths = []
-        for path in paths:
-            exclude = False
-            for tag in gents_config["exclude"]:
-                if tag in str(path):
-                    exclude = True
-                    break
-            if not exclude:
-                filtered_paths.append(path) 
-        paths = filtered_paths
-
-    check_config(gents_config)
-    path_to_meta_map = get_metas_from_paths(paths, dask_client=dask_client)
-    if gents_config["include"] is not None:
-        path_to_meta_map = apply_inclusive_filters(path_to_meta_map, gents_config["include"])
-
-    sliced_groups = {}
-    sliced_paths = []
-    
-    for tag in gents_config["include"]:
-        if tag != ".":
-            path_maps = {}
-            for path in path_to_meta_map:
-                if tag in path_to_meta_map[path].get_path():
-                    path_maps[path] = path_to_meta_map[path]
-                    sliced_paths.append(path)
-    
-            groups = sort_hf_groups(list(path_maps.keys()))
-            tag_groups = slice_hf_groups(groups, path_maps, gents_config["include"][tag]["chunk_years"])
-        
-            for group in tag_groups:
-                sliced_groups[group] = tag_groups[group]
-    
-    if "." in gents_config["include"]:
-        path_maps = {}
-        for path in path_to_meta_map:
-            if path not in sliced_paths:
-                path_maps[path] = path_to_meta_map[path]
-    
-        groups = sort_hf_groups(list(path_maps.keys()))
-        tag_groups = slice_hf_groups(groups, path_maps, gents_config["include"]["."]["chunk_years"])
-        for group in tag_groups:
-            sliced_groups[group] = tag_groups[group]
-    
-    group_metas = check_groups_by_variables(sliced_groups)
-    
-    return generate_time_series_from_meta_groups(group_metas, input_head_dir, output_head_dir, parallel_by_vars, complevel, compression, overwrite, dask_client)
+    return dask_client.compute(dask_gents, sync=True)
