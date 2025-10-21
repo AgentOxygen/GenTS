d5c7617a67381ed2d9d4f1e24d018f1fedfe13e6
AgentOxygen <cameron.cummins@utexas.edu>
2025-10-21 12:03:50 -0500

    Added test coverage for logging

--- gents/tests/test_utils.py
+++ gents/tests/test_utils.py
@@ -1,7 +1,46 @@
-from gents import utils
+from gents.utils import *
+import pytest
+
+LOGGER_HEADER_NUM_LINES = 2
+
 
 def test_timestamp():
-    assert type(utils.get_time_stamp()) == str
+    assert type(get_time_stamp()) == str
+
 
 def test_version():
-    assert type(utils.get_version()) == str
+    assert type(get_version()) == str
+
+
+@pytest.fixture(scope="session")
+def log_output_dir(tmp_path_factory):
+    return tmp_path_factory.mktemp("log_data")
+
+
+def test_logging(log_output_dir):
+    logger = logging.getLogger("gents")
+
+    enable_logging(verbose=False, output_path=f"{log_output_dir}/log_header.txt")
+
+    with open(f"{log_output_dir}/log_header.txt") as f:
+        assert len(f.readlines()) == LOGGER_HEADER_NUM_LINES
+
+    enable_logging(verbose=False, output_path=f"{log_output_dir}/non_verbose.txt")
+
+    logger.debug("test")
+    logger.info("test")
+    logger.warning("test")
+    logger.log(LOG_LEVEL_IO_WARNING, "test")
+
+    with open(f"{log_output_dir}/non_verbose.txt") as f:
+        assert len(f.readlines()) == LOGGER_HEADER_NUM_LINES + 3
+    
+    enable_logging(verbose=True, output_path=f"{log_output_dir}/verbose.txt")
+
+    logger.debug("test")
+    logger.info("test")
+    logger.warning("test")
+    logger.log(LOG_LEVEL_IO_WARNING, "test")
+
+    with open(f"{log_output_dir}/verbose.txt") as f:
+        assert len(f.readlines()) == LOGGER_HEADER_NUM_LINES + 4
