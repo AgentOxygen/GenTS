07bbd3b9ed8358e1aed28b5b69baa7144d1c9f73
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-12 15:17:45 -0500

    Removed dask cluster

--- gents/configs/gents_cesm3.py
+++ gents/configs/gents_cesm3.py
@@ -7,11 +7,7 @@ def run_config(args):
     gents.utils.enable_logging(verbose=args.verbose)
 
     client = None
-    if not args.serial:
-        from dask.distributed import LocalCluster, Client
-        cluster = LocalCluster(n_workers=args.numcores, threads_per_worker=1, memory_limit=f"{args.memorylimit}GB", processes=True)
-        client = cluster.get_client()
-    
+
     hf_collection = HFCollection(args.hf_head_dir, dask_client=client)
     hf_collection = hf_collection.include(["*/atm/*", "*/ice/*", "*/lnd/*", "*/glc/*", "*/rof/*"]).slice_groups(slice_size_years=args.slice)
     hf_collection.pull_metadata()
