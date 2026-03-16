16af1a293bf5daf6e1d4a16dff38214508ca7196
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-16 12:52:49 -0500

    Removed redundant metadata pull

--- gents/configs/gents_cesm3.py
+++ gents/configs/gents_cesm3.py
@@ -8,13 +8,12 @@ def run_config(args):
 
     hf_collection = HFCollection(args.hf_head_dir, num_processes=args.numcores)
     hf_collection = hf_collection.include(["*/atm/*", "*/ice/*", "*/lnd/*", "*/glc/*", "*/ocn/*", "*/rof/*"]).exclude(["*.ocean_geometry.nc", "*mom6.ic.*"]).slice_groups(slice_size_years=args.slice)
-    hf_collection.pull_metadata()
-    
     ts_collection = TSCollection(hf_collection, args.outputdir, num_processes=args.numcores).apply_path_swap("/hist/", "/proc/tseries/").append_timestep_dirs()
     if args.overwrite:
         ts_collection = ts_collection.apply_overwrite("*")
 
     if not args.dryrun:
         ts_collection.execute()
-
+    else:
+        print(f"{len(ts_collection)} timeseries files.")
     print("GenTS done!")
