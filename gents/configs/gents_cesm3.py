bf39662a99a4dbaa04c116a1e590ef797b48b8f3
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-17 08:00:54 -0500

    Excluded restart and log directories

--- gents/configs/gents_cesm3.py
+++ gents/configs/gents_cesm3.py
@@ -7,7 +7,7 @@ def run_config(args):
     gents.utils.enable_logging(verbose=args.verbose)
 
     hf_collection = HFCollection(args.hf_head_dir, num_processes=args.numcores)
-    hf_collection = hf_collection.include(["*/atm/*", "*/ice/*", "*/lnd/*", "*/glc/*", "*/ocn/*", "*/rof/*"]).exclude(["*.ocean_geometry.nc", "*mom6.ic.*"]).slice_groups(slice_size_years=args.slice)
+    hf_collection = hf_collection.include(["*/atm/*", "*/ice/*", "*/lnd/*", "*/glc/*", "*/ocn/*", "*/rof/*"]).exclude(["*/rest/*", "*/logs/*", "*.ocean_geometry.nc", "*mom6.ic.*"]).slice_groups(slice_size_years=args.slice)
     ts_collection = TSCollection(hf_collection, args.outputdir, num_processes=args.numcores).apply_path_swap("/hist/", "/proc/tseries/").append_timestep_dirs()
     if args.overwrite:
         ts_collection = ts_collection.apply_overwrite("*")
