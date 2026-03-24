7515cb398911b0895b550add4da214b364350671
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-24 14:00:59 -0500

    Added instantaneous cam output to exlcuded patterns

--- gents/configs/gents_cesm3.py
+++ gents/configs/gents_cesm3.py
@@ -2,12 +2,28 @@ from gents.hfcollection import HFCollection
 from gents.timeseries import TSCollection
 import gents
 
+INCLUDE_PATTERNS = [
+    "*/atm/*",
+    "*/ice/*",
+    "*/lnd/*",
+    "*/glc/*",
+    "*/ocn/*",
+    "*/rof/*"
+]
+
+EXCLUDE_PATTERNS = [
+    "*/rest/*",
+    "*/logs/*",
+    "*.ocean_geometry.nc",
+    "*mom6.ic.*",
+    "*cam.i.*"
+]
 
 def run_config(args):
     gents.utils.enable_logging(verbose=args.verbose)
 
     hf_collection = HFCollection(args.hf_head_dir, num_processes=args.numcores)
-    hf_collection = hf_collection.include(["*/atm/*", "*/ice/*", "*/lnd/*", "*/glc/*", "*/ocn/*", "*/rof/*"]).exclude(["*/rest/*", "*/logs/*", "*.ocean_geometry.nc", "*mom6.ic.*"]).slice_groups(slice_size_years=args.slice)
+    hf_collection = hf_collection.include(INCLUDE_PATTERNS).exclude(EXCLUDE_PATTERNS).slice_groups(slice_size_years=args.slice)
     ts_collection = TSCollection(hf_collection, args.outputdir, num_processes=args.numcores).apply_path_swap("/hist/", "/proc/tseries/").append_timestep_dirs()
     if args.overwrite:
         ts_collection = ts_collection.apply_overwrite("*")
