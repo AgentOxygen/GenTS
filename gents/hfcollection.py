1a1b6c30163098fe4514a9d7c9240ab1e3b1e51c
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-26 10:29:04 -0500

    Update docustring with typing

--- gents/hfcollection.py
+++ gents/hfcollection.py
@@ -658,9 +658,10 @@ class HFCollection:
 
         :param check_valid: If ``True`` (default), calls :meth:`check_validity`
             after loading to remove files with incomplete or invalid metadata.
+        :type check_valid: bool
         :param raise_errors: If ``True`` (default ``False``), calls errors are raised
             rather than just logged.
-        :type check_valid: bool
+        :type raise_errors: bool
         """
         logger.info(f"Pulling metadata...")
         paths = list(self.__hf_to_meta_map.keys())
