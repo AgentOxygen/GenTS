6f9f895e42a6bb0b5192331deb0441dc5ce23524
AgentOxygen <cameron.cummins@utexas.edu>
2026-03-31 13:41:43 -0500

    Added compression

--- gents/configs/gents_cesm3.py
+++ gents/configs/gents_cesm3.py
@@ -25,6 +25,7 @@ def run_config(args):
     hf_collection = HFCollection(args.hf_head_dir, num_processes=args.numcores)
     hf_collection = hf_collection.include(INCLUDE_PATTERNS).exclude(EXCLUDE_PATTERNS).slice_groups(slice_size_years=args.slice)
     ts_collection = TSCollection(hf_collection, args.outputdir, num_processes=args.numcores).apply_path_swap("/hist/", "/proc/tseries/").append_timestep_dirs()
+    ts_collection = ts_collection.apply_compression(2, "zstd", "*")
     if args.overwrite:
         ts_collection = ts_collection.apply_overwrite("*")
 
